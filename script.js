2cf8ec50f5a65e888f313bf7b0954b9cf40852da
vanilla <120879655+vanillauwuz@users.noreply.github.com>
2024-05-30 18:46:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -87,18 +87,3 @@ function declineButtonClick(event) {
 
 acceptButton.addEventListener("click", acceptButtonClick);
 declineButton.addEventListener("click", declineButtonClick);
-
-
-// var coll = document.getElementsByClassName("collapse-text");
-
-// for (var i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function () {
-//     this.classList.toggle("active");
-//     var content = this.nextElementSibling;
-//     if (content.style.display === "block") {
-//       content.style.display = "none";
-//     } else {
-//       content.style.display = "block";
-//     }
-//   });
-// }
